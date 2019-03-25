a2ba40c796b7471e2d966158b4dc02e542aab894
Шадикьян Руслан Артемович <@corp.itstep>
2019-03-25 18:27:21 +0600

    Star Wars

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using SharpTrooper.Core;
+using SharpTrooper.Entities;
+using System.Xml;
+
+namespace StarWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SharpTrooperCore sharpTrooper = new SharpTrooperCore();
+            Console.WriteLine("Введите идентификатор");
+            string id = Console.ReadLine();
+            if (int.TryParse(id, out int result))
+            {
+                People people = sharpTrooper.GetPeople(id);
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(People));
+
+                using (FileStream writer = new FileStream("file.xml", FileMode.OpenOrCreate))
+                {
+                    xmlSerializer.Serialize(writer, people);
+                }
+                Console.WriteLine(people.name);
+            }
+        }
+    }
+}
